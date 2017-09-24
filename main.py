08ea4dd901e2ba0035c6c61a667b2eb201893131
Adam Russo <adamrusso87@gmail.com>
2017-09-24 10:12:55 -0500

    form processing works

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="post">
+        <label for="rot">Rotate by:</label>
+            <input type="text" name="rot" value=0>
+            <textarea type="text" name="text"></textarea>
+            <input type="submit">
+        </form>
+    </body>
+</html>
+"""
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rotate_num = int(request.form['rot'])
+    initial_text = request.form['text']
+
+    encrypted_text = rotate_string(initial_text, rotate_num)
+    return "<h1>" + encrypted_text + "</h1>"
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
